4ef539dd4cc4da81da48d31dae666893845627a1
Dru1DD <mrtproger@gmail.com>
2024-09-02 19:07:38 +0200

    first revision of lending page

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Bakstag",
+  description: "Omnichain OTC Market",
+};
+
+export const viewport: Viewport = {
+  width: "device-width, shrink-to-fit=no",
+  minimumScale: 1,
+  initialScale: 1,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
